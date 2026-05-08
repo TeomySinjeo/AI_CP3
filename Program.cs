8f06486923550bb25e84f8fb9189bf7cb3ddb0a6
gavrilovrs24 <teomy.sinjeo@gmail.com>
2026-05-08 16:26:52 +0300

    Add this project named "AI_CP3"

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        using var client = new HttpClient();
+
+        while (true)
+        {
+            Console.Write("Ваш запрос: ");
+            var input = Console.ReadLine();
+
+            var request = new
+            {
+                model = "nemotron-3-super:cloud",
+                prompt = input,
+                stream = false
+            };
+
+            var json = JsonSerializer.Serialize(request);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("http://localhost:11434/api/generate", content);
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            using var doc = JsonDocument.Parse(responseString);
+            var result = doc.RootElement.GetProperty("response").GetString();
+
+            Console.WriteLine($"Nemo: {result}");
+        }
+    }
+}
